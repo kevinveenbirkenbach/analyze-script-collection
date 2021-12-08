ac44805a49bf50fcb7f56d92a77caed381647d26
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2021-12-08 12:46:47 +0100

    Made script indepentend from framework

--- file_duplicates.sh
+++ file_duplicates.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # @author Kevin Veen-Birkenbach
 # shellcheck source=/dev/null # Deactivate SC1090
-info "Checking relevant home folders for duplicated files..."
+echo "Checking relevant home folders for duplicated files..."
 fdupes -r "$HOME/Documents/" "$HOME/Downloads/" "$HOME/Images/" "$HOME/Desktop/" "$HOME/Music/" "$HOME/Pictures/" "$HOME/Videos"
